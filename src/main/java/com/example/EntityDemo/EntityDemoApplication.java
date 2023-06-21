81b8e5e44d8ac63e547f6d574d85e5e1bfecccae
Rudi <rudi.will.do.it@gmail.com>
2023-06-22 00:24:04 +0300

    init project

---
+++ src/main/java/com/example/EntityDemo/EntityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.EntityDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EntityDemo/EntityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EntityDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
